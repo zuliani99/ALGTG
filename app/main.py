51a1c2663fd29922f006b14d4769ccf5055df530
zuliani99 <875532@stud.unive.it>
2023-12-01 21:19:20 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from cifar10 import get_cifar10
 from utils import get_initial_dataloaders, get_resnet18, accuracy_score, plot_loss_curves
 from GTG_ActiveLearning import GTG_ActiveLearning
 
+
 save_plot = True
 
 
@@ -29,8 +30,8 @@ def main():
     optimizer = torch.optim.Adam(resnet18.parameters(), lr=0.001)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=2, verbose=True)
 
-    epochs = 1#30
-    al_iters = 2#50
+    epochs = 30
+    al_iters = 25
     top_k_obs = 1000
 
     GTG_AL = GTG_ActiveLearning(
@@ -53,7 +54,7 @@ def main():
     
 
 
-    results, n_lab_obs = GTG_AL.train_evaluate_AL_GTG(epochs=epochs, al_iters=al_iters, gtg_tol=0.001, gtg_max_iter=100, top_k_obs=top_k_obs, list_n_samples=[5, 10])#, 15, 20, 25, 30])
+    results, n_lab_obs = GTG_AL.train_evaluate_AL_GTG(epochs=epochs, al_iters=al_iters, gtg_tol=0.001, gtg_max_iter=100, top_k_obs=top_k_obs, list_n_samples=[5, 10, 15, 20])
 
     plot_loss_curves(results, n_lab_obs, save_plot, f'results_{epochs}_{al_iters}_{top_k_obs}.png')
     
