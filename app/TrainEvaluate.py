fix normalization + more
- added the plot for each loss
- added LL

app/TrainEvaluate.py
@@ -7,12 +7,10 @@ import copy
 from CIFAR10 import CIFAR10, Cifar10SubsetDataloaders
 from utils import get_mean_std
 
-import os
-
 
 class TrainEvaluate(object):
 
-    def __init__(self, params):
+    def __init__(self, params, LL):
         
         self.best_check_filename = 'app/checkpoints'
         self.init_check_filename = 'app/checkpoints/init_checkpoint.pth.tar'
@@ -39,6 +37,8 @@ class TrainEvaluate(object):
         self.optimizer = params['optimizer']
         self.scheduler = params['scheduler']
         self.loss_weird = params['loss_weird']
+        
+        self.LL = LL
             
         self.obtain_normalization()
         
@@ -53,8 +53,12 @@ class TrainEvaluate(object):
 
 
     def obtain_normalization(self):
+        self.original_trainset.flag_normalization = True
+
         mean, std = get_mean_std(self.lab_train_dl)
-        self.normalize = transforms.Compose([ transforms.Normalize(mean, std) ])  
+        self.normalize = transforms.Compose([ transforms.Normalize(mean, std) ])
+        
+        self.original_trainset.flag_normalization = False
         
         
         
@@ -86,9 +90,9 @@ class TrainEvaluate(object):
         
 
 
-    def evaluate(self, dataloader, flag_loss_weird = False):
-        tot_accuracy, tot_loss = .0, .0
-
+    def evaluate(self, dataloader, weight):
+        tot_loss, tot_loss_ce, tot_loss_weird, tot_accuracy = .0, .0, .0, .0
+        
         self.model.eval()
 
         with torch.inference_mode(): # Allow inference mode
@@ -100,10 +104,13 @@ class TrainEvaluate(object):
 
                 loss_ce = self.loss_fn(outputs, labels)
                 
-                if flag_loss_weird:    
+                if self.LL and weight:    
                     loss_weird = self.loss_weird(out_weird, loss_ce)
                     loss_ce = torch.mean(loss_ce)
                     loss = loss_ce + loss_weird
+                    
+                    tot_loss_ce += loss_ce
+                    tot_loss_weird += loss_weird
                 else:
                     loss = torch.mean(loss_ce)
                 
@@ -115,15 +122,16 @@ class TrainEvaluate(object):
 
             tot_accuracy /= len(dataloader)
             tot_loss /= len(dataloader)
+            tot_loss_ce /= len(dataloader)
+            tot_loss_weird /= len(dataloader)
             
-        return tot_accuracy, tot_loss
+        return tot_accuracy, tot_loss, tot_loss_ce, tot_loss_weird
 
 
 
 
     def train_evaluate(self, epochs, dataloader, method_str):
         
-        #resnet_weird
         weight = 1.   # 120 = 0
     
         check_best_path = f'{self.best_check_filename}/best_{method_str}.pth.tar'
@@ -132,20 +140,18 @@ class TrainEvaluate(object):
         actual_patience = 0
         
         
-        results = { 'train_loss': [], 'train_accuracy': [], 'val_loss': [], 'val_accuracy': [] }
+        results = { 'train_loss': [], 'train_loss_ce': [], 'train_loss_weird': [], 'train_accuracy': [], 
+                   'val_loss': [], 'val_loss_ce': [], 'val_loss_weird': [], 'val_accuracy': [] }
 	
 
         for epoch in range(epochs):
             
             self.model.train()
             
-            #resnet_weird
-            if epoch > 70: weight = 0
+            if epoch > 120: weight = 0
             
-            train_loss = 0.0
-            train_accuracy = 0.0
+            train_loss, train_loss_ce, train_loss_weird, train_accuracy = .0, .0, .0, .0
             
-
             for _, images, labels in dataloader:
 
                 # get the inputs; data is a list of [inputs, labels]
@@ -159,14 +165,15 @@ class TrainEvaluate(object):
                 
                 loss_ce = self.loss_fn(outputs, labels)
                     
-                loss_weird = self.loss_weird(out_weird, loss_ce)
-                loss_ce = torch.mean(loss_ce)
+                if self.LL and weight:
+                    loss_weird = self.loss_weird(out_weird, loss_ce)
+                    loss_ce = torch.mean(loss_ce)
+                    loss = loss_ce + loss_weird
                     
-                loss = loss_ce + weight * loss_weird
-                    
-                
-                #outputs, _, _, _ = self.model(images)
-                #loss = self.loss_fn(outputs, labels)
+                    train_loss_ce += loss_ce
+                    train_loss_weird += loss_weird
+                else:
+                    loss = torch.mean(loss_ce)
                 
                 
                 loss.backward()         
@@ -181,9 +188,11 @@ class TrainEvaluate(object):
 
             train_accuracy /= len(dataloader)
             train_loss /= len(dataloader)
+            train_loss_ce /= len(dataloader)
+            train_loss_weird /= len(dataloader)
             
             
-            val_accuracy, val_loss = self.evaluate(self.val_dl, weight)
+            val_accuracy, val_loss, val_loss_ce, val_loss_weird = self.evaluate(self.val_dl, weight)
             
             
             # CosineAnnealingLR
@@ -192,8 +201,14 @@ class TrainEvaluate(object):
             
 
             results['train_loss'].append(train_loss)
+            results['train_loss_ce'].append(train_loss_ce)
+            results['train_loss_weird'].append(train_loss_weird)
             results['train_accuracy'].append(train_accuracy)
+            
+            
             results['val_loss'].append(val_loss)
+            results['val_loss_ce'].append(val_loss_ce)
+            results['val_loss_weird'].append(val_loss_weird)
             results['val_accuracy'].append(val_accuracy)
             
             
@@ -224,7 +239,7 @@ class TrainEvaluate(object):
                 epoch + 1, train_accuracy, train_loss, val_accuracy, val_loss, best_val_loss))
 
 
-            if epoch == 70:
+            if epoch == 160:
                 print('Decreasing learning rate to 0.01 and ignoring the learning loss\n')
                 for g in self.optimizer.param_groups: g['lr'] = 0.01
                     

app/utils.py
@@ -20,20 +20,23 @@ def save_init_checkpoint(model, optimizer, scheduler):
 
 
 def get_mean_std(dataloader):
-    mean = 0.0
-    std = 0.0
+    
+    cnt = 0
+    fst_moment = torch.empty(3)
+    snd_moment = torch.empty(3)
 
     for _, images, _ in dataloader:
-        # Assuming images are in the shape (batch_size, channels, height, width)
-        batch_samples = images.size(0) 
-        images = images.view(batch_samples, images.size(1), -1)
-        mean += images.mean(2).sum(0)
-        std += images.std(2).sum(0)
+        b, c, h, w = images.shape
+        nb_pixels = b * h * w
+        sum_ = torch.sum(images, dim=[0, 2, 3])
+        sum_of_square = torch.sum(images ** 2, dim=[0, 2, 3])
+        fst_moment = (cnt * fst_moment + sum_) / (cnt + nb_pixels)
+        snd_moment = (cnt * snd_moment + sum_of_square) / (cnt + nb_pixels)
+        cnt += nb_pixels
 
-    mean /= len(dataloader.dataset)
-    std /= len(dataloader.dataset)
-    
-    return mean, std
+    mean, std = fst_moment, torch.sqrt(snd_moment - fst_moment ** 2)        
+    return mean,std
+
     
 
 def accuracy_score(output, label):
@@ -50,7 +53,10 @@ def entropy(tensor):
 
 def plot_loss_curves(methods_results, n_lab_obs, save_plot, ts_dir, plot_png_name = None):
     
-    _, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (18,8))
+    
+    _, ax = plt.subplots(nrows = 2, ncols = 2, figsize = (18,8))
+    
+    
     
     for method_str, values in methods_results.items():
         if(isinstance(list(values.keys())[0], int)):
@@ -60,9 +66,9 @@ def plot_loss_curves(methods_results, n_lab_obs, save_plot, ts_dir, plot_png_nam
             ax[0].plot(n_lab_obs, values['test_loss'], label = f'{method_str}')
 
         
-    ax[0].set_title('Loss - # Labeled Obs')
+    ax[0].set_title('Total Loss - # Labeled Obs')
     ax[0].set_xlabel('# Labeled Obs')
-    ax[0].set_ylabel('Loss')
+    ax[0].set_ylabel('Total Loss')
     ax[0].grid()
     ax[0].legend()
 
@@ -93,36 +99,65 @@ def save_train_val_curves(results_info, ts_dir, al_iter):
     res = results_info['results']
     epochs = range(1, len(res['train_loss']) + 1)
 
-    fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (18,8))
+    fig, ax = plt.subplots(nrows = 2, ncols = 2, figsize = (18,8))
     minloss_val = min(res['val_loss'])
     minloss_ep = res['val_loss'].index(minloss_val) + 1
 
     maxacc_ep = res['val_accuracy'][minloss_ep - 1]
     
 
-    ax[0].plot(epochs, res['train_loss'], label = 'train_loss')
-    ax[0].plot(epochs, res['val_loss'], label = 'val_loss')
-    ax[0].set_ylim([0, 5])
-    ax[0].axvline(minloss_ep, linestyle='--', color='r', label='Early Stopping Checkpoint')
 
-    ax[0].axhline(minloss_val, linestyle='--', color='r')
-    ax[0].set_title('Loss - Epochs')
-    ax[0].set_xlabel('Epochs')
-    ax[0].set_ylabel('Loss')
-    ax[0].grid()
-    ax[0].legend()
+    ax[0][0].plot(epochs, res['train_loss'], label = 'train_loss')
+    ax[0][0].plot(epochs, res['val_loss'], label = 'val_loss')
+    ax[0][0].set_ylim([0, 5])
+    ax[0][0].axvline(minloss_ep, linestyle='--', color='r', label='Early Stopping Checkpoint')
+
+    ax[0][0].axhline(minloss_val, linestyle='--', color='r')
+    ax[0][0].set_title('Total Loss - Epochs')
+    ax[0][0].set_xlabel('Epochs')
+    ax[0][0].set_ylabel('Total Loss')
+    ax[0][0].grid()
+    ax[0][0].legend()
 
 
-    ax[1].plot(epochs, res['train_accuracy'], label = 'train_accuracy_score')
-    ax[1].plot(epochs, res['val_accuracy'], label = 'val_accuracy_score')
-    ax[1].axvline(minloss_ep, linestyle='--', color='r', label='Early Stopping Checkpoint')
+    ax[0][1].plot(epochs, res['train_accuracy'], label = 'train_accuracy_score')
+    ax[0][1].plot(epochs, res['val_accuracy'], label = 'val_accuracy_score')
+    ax[0][1].axvline(minloss_ep, linestyle='--', color='r', label='Early Stopping Checkpoint')
 
-    ax[1].axhline(maxacc_ep, linestyle='--', color='r')
-    ax[1].set_title('Accuracy Score - Epochs')
-    ax[1].set_ylabel('Accuracy Score')
-    ax[1].set_xlabel('Epochs')
-    ax[1].grid()
-    ax[1].legend()
+    ax[0][1].axhline(maxacc_ep, linestyle='--', color='r')
+    ax[0][1].set_title('Accuracy Score - Epochs')
+    ax[0][1].set_ylabel('Accuracy Score')
+    ax[0][1].set_xlabel('Epochs')
+    ax[0][1].grid()
+    ax[0][1].legend()
+    
+    
+    
+    ax[1][0].plot(epochs, res['train_loss_ce'], label = 'train_loss_ce')
+    ax[1][0].plot(epochs, res['val_loss_ce'], label = 'val_loss_ce')
+    ax[1][0].axvline(minloss_ep, linestyle='--', color='r', label='Early Stopping Checkpoint')
+
+    ax[1][0].axhline(maxacc_ep, linestyle='--', color='r')
+    ax[1][0].set_title('CE Loss - Epochs')
+    ax[1][0].set_ylabel('CE Loss')
+    ax[1][0].set_xlabel('Epochs')
+    ax[1][0].grid()
+    ax[1][0].legend()
+    
+    
+    
+    ax[1][1].plot(epochs, res['train_loss_weird'], label = 'train_loss_weird')
+    ax[1][1].plot(epochs, res['val_loss_weird'], label = 'val_loss_weird')
+    ax[1][1].axvline(minloss_ep, linestyle='--', color='r', label='Early Stopping Checkpoint')
+
+    ax[1][1].axhline(maxacc_ep, linestyle='--', color='r')
+    ax[1][1].set_title('Loss Weird - Epochs')
+    ax[1][1].set_ylabel('Loss Weird')
+    ax[1][1].set_xlabel('Epochs')
+    ax[1][1].grid()
+    ax[1][1].legend()
+    
+    
     plt.suptitle(f'AL iter {al_iter} - {results_info["model_name"]}', fontsize = 30)
     
     if(not os.path.exists(f'results/{ts_dir}/train_val_plots/{results_info["model_name"]}')):
@@ -176,8 +211,8 @@ def init_params_fn(net):
         #elif isinstance(m, BasicBlock):
         #    for c in list(m.children()): init_params_apply(c)
         
-        
 
+# weights initiaization
 def init_params_apply(m):
     if isinstance(m, nn.Conv2d):
         init.kaiming_normal_(m.weight, mode='fan_out')