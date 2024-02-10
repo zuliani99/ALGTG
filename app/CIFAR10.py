fix normalization

app/CIFAR10.py
@@ -5,26 +5,23 @@ from torchvision import datasets
 from torchvision import transforms
 import torch
 
-import numpy as np
-
 
 class Cifar10SubsetDataloaders():
     
-    def __init__(self, batch_size, val_rateo, labeled_ratio, normalize_train = False):
+    def __init__(self, batch_size, val_rateo, labeled_ratio, normalize_train):
         self.batch_size = batch_size
-        
-        self.original_trainset = CIFAR10(bool_train=True)
-        # heare there are the indices of the labeled observation to transform
         
         # flag to decide the normalization procedure
         self.normalize_train = normalize_train
         
-        self.test_dl = DataLoader(CIFAR10(bool_train=False), self.batch_size, shuffle=False,  pin_memory=True)
+        self.original_trainset = CIFAR10(bool_train=True)
+        # here there are the indices of the labeled observation to transform
+        
+        self.test_dl = DataLoader(CIFAR10(bool_train=False), self.batch_size, shuffle=False, pin_memory=True)
 
         self.classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
     
         self.get_initial_dataloaders(val_rateo, labeled_ratio)
-        if self.normalize_train: self.original_trainset.get_std_mean_train()
     
     
     
@@ -38,7 +35,7 @@ class Cifar10SubsetDataloaders():
         train_data, val_data = random_split(self.original_trainset, [int(new_train_size), int(val_size)])
 
         # validation dataloader
-        self.val_dl = DataLoader(val_data, batch_size=self.batch_size, shuffle=False,  pin_memory=True)
+        self.val_dl = DataLoader(val_data, batch_size=self.batch_size, shuffle=False, pin_memory=True)
                 
         # Calculate the number of samples for each split
         labeled_size = int(labeled_ratio * new_train_size)
@@ -51,28 +48,34 @@ class Cifar10SubsetDataloaders():
         self.unlab_train_subset = Subset(self.original_trainset, [train_data.indices[id] for id in unlabeled_set.indices])
         
         # Obtain the splitted dataloader
-        self.lab_train_dl = DataLoader(labeled_set, batch_size=self.batch_size, shuffle=True,  pin_memory=True)
+        self.lab_train_dl = DataLoader(labeled_set, batch_size=self.batch_size, shuffle=True, pin_memory=True)
     
         self.original_trainset.lab_train_idxs = self.lab_train_subset.indices
-
+            
     
-
 
 class CIFAR10(Dataset):
     def __init__(self, bool_train):
         self.cifar10 = datasets.CIFAR10('./cifar10', train=bool_train, download=True)
-        
-        self.transform_labeled = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-        ])
-        self.transform = transforms.Compose( [transforms.ToTensor()] )
-        
+                      
         self.lab_train_idxs = None
         self.flag_normalization = False
         
         self.bool_train = bool_train
+        
+        self.transform_labeled = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.4913997551666284, 0.48215855929893703, 0.4465309133731618],
+                                    [0.24703225141799082, 0.24348516474564, 0.26158783926049628])
+        ])
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.4913997551666284, 0.48215855929893703, 0.4465309133731618],
+                                [0.24703225141799082, 0.24348516474564, 0.26158783926049628])
+        ] )
+            
         
         
     def __len__(self):
@@ -80,26 +83,15 @@ class CIFAR10(Dataset):
 
     def __getitem__(self, index):
         image, label = self.cifar10[index]
-                
+        
         if not self.flag_normalization and self.bool_train and index in self.lab_train_idxs:
             image = self.transform_labeled(image)
         else:
             image = self.transform(image)
             
         return index, image, label
-    
-    
-    def get_std_mean_train(self):
-        # not necessarly since I acces to self.cifar10 with is the original dataset and not from my overload of Dataset
-        #self.flag_normalization = True
-        x = np.concatenate([np.asarray(self.cifar10[i][0]) for i in range(len(self.cifar10))])
-        #self.flag_normalization = False
-        
-        # calculate the mean and std along the (0, 1) axes
-        self.train_mean = np.mean(x, axis=(0, 1)) / 255
-        self.train_std = np.std(x, axis=(0, 1)) / 255
-        
-    
+
+
 
 class UniqueShuffle(Sampler):
     def __init__(self, dataset):

app/ResNet18.py
@@ -5,22 +5,24 @@ import torch.nn.functional as F
 import torchvision.models as models
 
 
-
 class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
+        if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
             )
 
     def forward(self, x):
@@ -38,16 +40,17 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512 * block.expansion, num_classes)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
+        strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
@@ -61,9 +64,10 @@ class ResNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
-        fc7 = out.view(out.size(0), -1)
-        out = self.linear(fc7)
-        return out, fc7, None, None
+        embeds = out.view(out.size(0), -1)
+        out = self.linear(embeds)
+        return out, embeds, None, None
+
     
 
 
@@ -121,7 +125,6 @@ class ResNet_Weird(nn.Module):
 
 
 
-
 class LearningLoss(nn.Module):
 
     def __init__(self, device, margin=1):
@@ -148,6 +151,7 @@ class LearningLoss(nn.Module):
         greater_than_zero = greater_than_zero.to(self.device)
         loss = calc_loss * greater_than_zero
         return torch.mean(loss)
+
 
 
 

app/TrainEvaluate.py
@@ -42,15 +42,7 @@ class TrainEvaluate(object):
         
         self.LL = LL
         
-        if self.normalize_train:
-            print(' => Setting the normalize with the mean and std from the entire train set')
-            # normalization using the entire train mean and std
-            self.normalize = transforms.Compose([ transforms.Normalize(
-                self.original_trainset.train_mean, self.original_trainset.train_std
-            ) ])
-            print(' DONE\n')
-        else:
-            self.obtain_normalization()
+        if not self.normalize_train: self.obtain_normalization()
 
             
     def reintialize_model(self): self.__load_init_checkpoint()
@@ -108,7 +100,8 @@ class TrainEvaluate(object):
         with torch.inference_mode(): # Allow inference mode
             for _, images, labels in dataloader:
                 
-                images, labels = self.normalize(images.to(self.device)), labels.to(self.device)
+                images, labels = images.to(self.device), labels.to(self.device)
+                if not self.normalize_train: self.normalize(images)
 
                 outputs, _, out_weird, _ = self.model(images)
 
@@ -165,7 +158,8 @@ class TrainEvaluate(object):
             for _, images, labels in dataloader:
 
                 # get the inputs; data is a list of [inputs, labels]
-                images, labels = self.normalize(images.to(self.device)), labels.to(self.device)
+                images, labels = images.to(self.device), labels.to(self.device)
+                if not self.normalize_train: self.normalize(images)
                 
                 self.optimizer.zero_grad()
                                 
@@ -280,8 +274,8 @@ class TrainEvaluate(object):
         with torch.inference_mode():
             for _, images, labels in dataloader:
                 
-                images = self.normalize(images.to(self.device))
-                labels = labels.to(self.device)
+                images, labels = images.to(self.device), labels.to(self.device)
+                if not self.normalize_train: self.normalize(images)
                 
                 _, embed, _, _ = self.model(images)
                 
@@ -295,19 +289,18 @@ class TrainEvaluate(object):
     def get_new_dataloaders(self, overall_topk):
         
         # temp variable
-        lab_train_indices = self.lab_train_subset.indices
+        lab_train_indices = copy.deepcopy(self.lab_train_subset.indices)
         
         # extend with the overall_topk
         lab_train_indices.extend(overall_topk)
         # generate a new Subset
         self.lab_train_subset = Subset(self.original_trainset, lab_train_indices)
         
-        
         # update the indices for the transform        
         self.original_trainset.lab_train_idxs = lab_train_indices
-        
+            
         # temp variable
-        unlab_train_indices = self.unlab_train_subset.indices
+        unlab_train_indices = copy.deepcopy(self.unlab_train_subset.indices)
         # remove new labeled observations
         for idx_to_remove in overall_topk: unlab_train_indices.remove(idx_to_remove)
         # generate a new Subset
@@ -321,6 +314,5 @@ class TrainEvaluate(object):
         # generate the new labeled DataLoader
         self.lab_train_dl = DataLoader(self.lab_train_subset, batch_size=self.batch_size, shuffle=True,  pin_memory=True)
         
-        if not self.normalize_train: 
-            self.obtain_normalization()
+        if not self.normalize_train: self.obtain_normalization()
         

app/main.py
@@ -74,8 +74,9 @@ def main():
     cifar10 = Cifar10SubsetDataloaders(batch_size, val_rateo = 0.2, labeled_ratio = 0.025, normalize_train = normalize_train)
     
     model = ResNet_Weird(BasicBlock, [2, 2, 2, 2])
-    #model.apply(init_params_apply)
-    init_params_fn(model)
+    #model = ResNet(BasicBlock, [2, 2, 2, 2])
+    model.apply(init_params_apply)
+    #init_params_fn(model)
     
     
     optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)

app/methods/Class_Entropy.py
@@ -28,7 +28,9 @@ class Class_Entropy(TrainEvaluate):
         with torch.inference_mode(): # Allow inference mode
             for idxs, images, _ in self.unlab_train_dl:
                 
-                idxs, images = idxs.to(self.device), self.normalize(images.to(self.device))
+                #idxs, images = idxs.to(self.device), self.normalize(images.to(self.device))
+                idxs, images = idxs.to(self.device), images.to(self.device)
+                if not self.normalize_train: self.normalize(images)
                 
                 outputs, _, _, _ = self.model(images)
                     