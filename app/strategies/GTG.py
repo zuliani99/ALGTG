fix gtg entropy derivatives

app/strategies/GTG.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader, Subset
 import torch.nn.functional as F
 
 from scipy.integrate import simpson #,trapz
-from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
 import gc
@@ -61,8 +61,12 @@ class GTG(Strategies):
         elif self.strategy_type == 'mixed':    
             # set the unlabeled submatrix as distance matrix and not similarity matrix
             n_lab_obs = len(self.lab_embedds_dict['embedds'])
-            A[:n_lab_obs, n_lab_obs:] = 1 - A[:n_lab_obs, n_lab_obs:]
-            A[n_lab_obs:, :n_lab_obs] = 1 - A[n_lab_obs:, :n_lab_obs]
+            if self.A_function == 'e_d':
+                A[:n_lab_obs, :n_lab_obs] = 1 - A[:n_lab_obs, :n_lab_obs]
+                A[n_lab_obs:, n_lab_obs:] = 1 - A[n_lab_obs:, n_lab_obs:]
+            else:   
+                A[:n_lab_obs, n_lab_obs:] = 1 - A[:n_lab_obs, n_lab_obs:]
+                A[n_lab_obs:, :n_lab_obs] = 1 - A[n_lab_obs:, :n_lab_obs]
             
             # Unlabeled VS Unlabeled -> similarity = A
             # Labeled VS Labeled -> similarity = A
@@ -70,6 +74,7 @@ class GTG(Strategies):
             # Labeled VS Unlabeled -> distance = 1 - A
         
         # remove weak connections 
+        # see how much it is infuent on the results
         A_2 = torch.where(A < 0.5, 0, A)
             
         self.A = A_2
@@ -115,24 +120,24 @@ class GTG(Strategies):
         
         A = torch.cdist(concat_embedds, concat_embedds).to(self.device)
         
-        if self.zero_diag: A.fill_diagonal_(0.)
-        else: A.fill_diagonal_(1.)
+        #if self.zero_diag: A.fill_diagonal_(0.)
+        #else: A.fill_diagonal_(1.)
         return A
 
 
 
     # correct
     def get_A_cos_sim(self, concat_embedds: torch.Tensor) -> torch.Tensor:
-                                
-        #normalized_embedding = F.normalize(concat_embedds, dim=-1).to(self.device)
+                               
+        normalized_embedding = F.normalize(concat_embedds, dim=-1).to(self.device)
         
-        #A = F.relu(torch.matmul(normalized_embedding, normalized_embedding.transpose(-1, -2)).to(self.device))
+        A = F.relu(torch.matmul(normalized_embedding, normalized_embedding.transpose(-1, -2)).to(self.device))
         
-        #if self.zero_diag: A.fill_diagonal_(0.)
-        #else: A.fill_diagonal_(1.)
-        A = cosine_similarity(concat_embedds.cpu().numpy())
-        return torch.from_numpy(A).to(self.device)
-        #return A
+        if self.zero_diag: A.fill_diagonal_(0.)
+        else: A.fill_diagonal_(1.)
+        #A = cosine_similarity(concat_embedds.cpu().numpy())
+        #return torch.from_numpy(A).to(self.device)
+        return A
         
         
         
@@ -148,18 +153,16 @@ class GTG(Strategies):
 
     # correct
     def get_X(self) -> None:
-        
-        len_samp_unlab_embeds = len(self.idx_unlabeled)
-        
+                
         self.X: torch.Tensor = torch.zeros(
-            (len(self.labeled_indices) + len_samp_unlab_embeds, self.n_classes),
+            (len(self.labeled_indices) + self.len_unlab_sample, self.n_classes),
             dtype=torch.float32,
             device=self.device
         )
 
         for idx, label in enumerate(self.lab_embedds_dict['labels']): self.X[idx][label.item()] = 1.
         
-        for idx in range(len(self.labeled_indices), len(self.labeled_indices) + len_samp_unlab_embeds):
+        for idx in range(len(self.labeled_indices), len(self.labeled_indices) + self.len_unlab_sample):
             for label in range(self.n_classes): self.X[idx][label] = 1. / self.n_classes
         
         
@@ -195,8 +198,8 @@ class GTG(Strategies):
             # I have to map only the sample_unlabeled to the correct position
             
             # Update only the unlabeled observations
-            assert len(self.entropy_history_unlabeled[:,i]) == len(iter_entropy[len(self.labeled_indices):]), 'should have the same dimension'
-            self.entropy_history_unlabeled[:,i] = iter_entropy[len(self.labeled_indices):]
+            assert len(self.unlab_entropy_hist[:,i]) == len(iter_entropy[len(self.labeled_indices):]), 'Should have the same dimension'
+            self.unlab_entropy_hist[:,i] = iter_entropy[len(self.labeled_indices):]
             # they have the same dimension
                         
             err = torch.norm(self.X - X_old)
@@ -205,37 +208,33 @@ class GTG(Strategies):
 
 
     def query(self, sample_unlab_subset: Subset, n_top_k_obs: int) -> List[int]:
-        #gc.collect()
-        #torch.cuda.empty_cache()
             
         # set the entire batch size to the dimension of the sampled unlabeled set
-        unlab_batch_size = len(sample_unlab_subset)
+        self.len_unlab_sample = len(sample_unlab_subset)
 
         # we have the batch size which is equal to the number of sampled observation from the unlabeled set                    
-        self.unlab_train_dl = DataLoader(                                       # set shuffle to false since I do not have interest on shufflind the dataloader, since I have only to get the embeddings
-            sample_unlab_subset, batch_size=unlab_batch_size,                   # thus there is no needs on shuffling the unlabeled dataloader
+        self.unlab_train_dl = DataLoader(                           # set shuffle to false since I do not have interest on shufflind the dataloader, since I have only to get the embeddings
+            sample_unlab_subset, batch_size=self.len_unlab_sample,  # thus there is no needs on shuffling the unlabeled dataloader
             shuffle=False, pin_memory=True
-            
         )
                 
         print(' => Getting the labeled and unlabeled embeddings')
         self.lab_embedds_dict = {'embedds': None, 'labels': None}
-        self.unlab_embedds_dict = {'embedds': None}#, 'labels': None, 'idxs': None}
+        self.unlab_embedds_dict = {'embedds': None}
             
         self.get_embeddings(self.lab_train_dl, self.lab_embedds_dict)
         self.get_embeddings(self.unlab_train_dl, self.unlab_embedds_dict)
         print(' DONE\n')
             
-        # without using cuda memory
+        # i can take the indices and labels fromt he dataloader, without using the get_embeddings function so no cuda memory is used in this case
+        # I have a single batch
         self.idx_unlabeled = next(iter(self.unlab_train_dl))[0]
         self.labels_unlabeled = next(iter(self.unlab_train_dl))[2]
             
             
         # I save the entropy history in order to be able to plot it
-        self.entropy_history_unlabeled = torch.zeros((unlab_batch_size, self.gtg_max_iter), device=self.device)
+        self.unlab_entropy_hist = torch.zeros((self.len_unlab_sample, self.gtg_max_iter), device=self.device)
         
-        #labels: List[int] = self.lab_embedds_dict['labels'].cpu().tolist()
-        #labels.extend(self.labels_unlabeled.tolist())
             
         print(' => Execution of the Graph Trasduction Game')
         self.graph_trasduction_game()
@@ -248,81 +247,58 @@ class GTG(Strategies):
         del self.unlab_embedds_dict
         torch.cuda.empty_cache()         
         
+        
         path = f'results/{self.timestamp}/{self.dataset_name}/{self.samp_iter}/gtg_entropies_plots'
         create_directory(path)
         
-        print(f' => Extracting the Top-k unlabeled observation using {self.ent_strategy}')
+        # plot entropy history
+        plot_history(path, self.labels_unlabeled.tolist(), self.classes, self.method_name, self.unlab_entropy_hist, self.iter - 1, self.gtg_max_iter)
+        
+        print(f' => Extracting the Top-k unlabeled observations using {self.ent_strategy}')
             
-        if self.ent_strategy is Entropy_Strategy.LAST:
-            # returning the last entropies values
-            overall_topk = torch.topk(self.entropy_history_unlabeled[-1], n_top_k_obs)
         
         if self.ent_strategy is Entropy_Strategy.H_INT:
-            # computing the area of each entropies derivates fucntion via the trapezius formula 
-            #area: np.ndarray = trapz(-np.diff(self.entropy_history.cpu().numpy(), axis=1))
-            #area: np.ndarray = simpson(-np.diff(self.entropy_history.cpu().numpy(), axis=1))
-            
-            #-----------------------------------------------------------------------------------------------
-            # WRONG COMPUTING THE AREA OVER THE DERIVATIVES OF THE ENTROPY
-            # I WANT THE ARE OF THE ORIGINAL FUNCTION ENTROPIES THUS THE THEIR HISOTRY OVER GTG ITERATIONS
-            area: np.ndarray = simpson(self.entropy_history_unlabeled.cpu().numpy())
-            #-----------------------------------------------------------------------------------------------
+            # computing the area of each entropies derivates fucntion via the simpson formula 
+            area: np.ndarray = simpson(self.unlab_entropy_hist.cpu().numpy())
                         
-            overall_topk = torch.topk(torch.from_numpy(area), n_top_k_obs)
+            overall_topk = torch.topk(torch.from_numpy(area), k=n_top_k_obs, largest=True)
+        
+        elif self.ent_strategy is Entropy_Strategy.DER:
+            # compute the pairwise differences to obtaion the entropy derivatives
+            self.unlab_entropy_der = -torch.diff(self.unlab_entropy_hist, dim=1)
+            
+            negative_indices = (self.unlab_entropy_der < 0).nonzero()
+            rows, cols = negative_indices.unbind(1)
+            
+            first_negative = torch.full((1, self.len_unlab_sample), -1).squeeze()
+            last_negative = torch.full((1, self.len_unlab_sample), -1).squeeze()
+            
+            for row, col in zip(rows, cols):
+                if first_negative[row] == -1: first_negative[row] = col
+                last_negative[row] = col
+                
+            for row, (first_0, last_0) in enumerate(zip(first_negative, last_negative)):
+                for idx in range(first_0+1, last_0):
+                    self.unlab_entropy_der[row, idx] = -torch.abs(self.unlab_entropy_der[row, idx])
+                                
+            overall_topk = torch.topk(torch.sum(self.unlab_entropy_der, dim=1), k=n_top_k_obs, largest=False)
+        
+            plot_derivatives(
+                self.method_name, self.unlab_entropy_der,
+                path, self.labels_unlabeled.tolist(), self.classes, self.iter - 1, self.gtg_max_iter - 1, 'derivatives'
+            )
+        
+            del self.unlab_entropy_der
+            torch.cuda.empty_cache()
         
         else:
-            # absolute value of the derivate to remove the oscilaltions -> observaion that have oscillations means that are difficult too
-            self.entropy_pairwise_unlabeled_der = torch.abs(-torch.diff(self.entropy_history_unlabeled, dim=1))
+            raise Exception('Unrecognized derivates computation strategy')
             
-            # plot entropy history
-            plot_history(path, self.labels_unlabeled.tolist(), self.classes, self.method_name, self.entropy_history_unlabeled, self.iter - 1, self.gtg_max_iter)
-
-            if self.ent_strategy is Entropy_Strategy.W_A_DER:
-                # getting the last column that have at least one element with entropy greater than 1e-15
-                for col_index in range(self.entropy_pairwise_unlabeled_der.size(1) - 1, -1, -1):
-                    if torch.any(self.entropy_pairwise_unlabeled_der[:, col_index] > 1e-15): break
-                    
-                    
-                # set the weights to increasing value until col_index
-                weights = torch.zeros(self.gtg_max_iter - 1, dtype=torch.float32, device=self.device) 
-                weights[:col_index] = torch.flip(torch.linspace(1, 0.1, col_index), [0]).to(self.device)
-                
-                
-                # weighted average excluding the derivates that are bellow the threshold
-                overall_topk = torch.topk(
-                    torch.sum(self.entropy_pairwise_unlabeled_der * weights, dim = 1) / col_index,
-                    n_top_k_obs
-                )    
-            
-            elif self.ent_strategy is Entropy_Strategy.MEAN_DER:
-                # classic average among the derivates
-                overall_topk = torch.topk(torch.mean(self.entropy_pairwise_unlabeled_der, dim=1), n_top_k_obs)
-            else:
-                raise Exception('Unrecognized derivates computation strategy')
-            
-            # plot in the entropy derivatives and weighted entropy derivatives
-            plot_derivatives(
-                self.method_name, self.entropy_pairwise_unlabeled_der,
-                self.entropy_pairwise_unlabeled_der * weights if self.ent_strategy is Entropy_Strategy.W_A_DER else self.entropy_pairwise_unlabeled_der,
-                path, self.labels_unlabeled.tolist(), self.classes, self.iter - 1, self.gtg_max_iter - 1, 'weighted_derivatives'
-            )
-            
-            # plot in the top k entropy derivatives and weighted entropy derivatives
-            plot_derivatives(
-                self.method_name, self.entropy_pairwise_unlabeled_der[overall_topk.indices.tolist()],
-                (self.entropy_pairwise_unlabeled_der * weights if self.ent_strategy is Entropy_Strategy.W_A_DER else self.entropy_pairwise_unlabeled_der)[overall_topk.indices.tolist()],
-                path, self.labels_unlabeled.tolist(), self.classes, self.iter - 1, self.gtg_max_iter - 1, 'topk_weighted_derivatives'
-            )
-            
-        
-            del self.entropy_pairwise_unlabeled_der
-            torch.cuda.empty_cache()  
-            
-        del self.entropy_history_unlabeled
+        del self.unlab_entropy_hist
         gc.collect()
         torch.cuda.empty_cache() 
         
         print(' DONE\n')
         
-        #return overall_topk.indices.tolist()
         return [self.idx_unlabeled[id].item() for id in overall_topk.indices.tolist()]
+    

app/strategies/Strategies.py
@@ -1,4 +1,5 @@
 
+from utils import count_class_observation
 from train_evaluate.TrainEvaluate import TrainEvaluate
 
 from torch.utils.data import Subset
@@ -43,15 +44,15 @@ class Strategies(TrainEvaluate):
             print(f'Index: {idx} \t Last 5 observations: {seq[-5:]}')
             
             unlabeled_subset = Subset(self.non_transformed_trainset, seq)
-            d_labels = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
             
-            for _, _, lab in unlabeled_subset: d_labels[str(lab)] += 1
+            d_labels = count_class_observation(self.classes, unlabeled_subset)
             
             self.unlab_sampled_list.append(unlabeled_subset)
             for x in seq: self.unlabeled_indices.remove(x)
-            print(d_labels)
+            print('Observations per class', d_labels)
+            print('\n')
         
-        print(f'Lenght of the unlabeled sampled subset list: {len(self.unlab_sampled_list)}')
+        print(f'Lenght of the unlabeled sampled subset list: {len(self.unlab_sampled_list)}\n')
         random.seed(10001) # reset the random seed
         del self.unlabeled_indices
         
@@ -90,14 +91,8 @@ class Strategies(TrainEvaluate):
             # run method query strategy
             topk_idx_obs: List[int] = self.query(self.unlab_sampled_list[idx_list], self.n_top_k_obs)
             
-            label_topk = [self.transformed_trainset[k][2] for k in topk_idx_obs]
-            d_labels = {}
-            for cls in self.classes: d_labels[cls] = 0
-            keys_d_labels = list(d_labels.keys())
-            for l in label_topk: d_labels[keys_d_labels[l]] += 1
-            print('Number of observations per class added to the labeled set:')
-            print(d_labels)
-            
+            d_labels = count_class_observation(self.classes, self.transformed_trainset, topk_idx_obs)
+            print(f' Number of observations per class added to the labeled set:\n {d_labels}\n')
             
             # get the new labeled indices from the subset to plot the tsne embeddings
             indices_unlab = [self.unlab_sampled_list[idx_list].indices.index(item) for item in topk_idx_obs]

app/strategies/baselines/Entropy.py
@@ -25,14 +25,15 @@ class Entropy(Strategies):
             batch_size=self.batch_size, shuffle=False, pin_memory=True
         )
                 
-        print(' => Evalueting unlabeled observations')
+        print(' => Evaluating unlabeled observations')
         embeds_dict = {'probs': None, 'idxs': None}
         self.get_embeddings(self.unlab_train_dl, embeds_dict)
         prob_dist = F.softmax(embeds_dict['probs'], dim=1)
         print(' DONE\n')
-                
+
+        print(f' => Extracting the Top-k unlabeled observations')
         tot_entr = entropy(prob_dist).to(self.device)
         overall_topk = torch.topk(tot_entr, n_top_k_obs)
-        
+        print(' DONE\n')
         
         return [embeds_dict['idxs'][id].item() for id in overall_topk.indices.tolist()]

app/train_evaluate/TrainEvaluate.py
@@ -63,24 +63,22 @@ class TrainEvaluate(object):
         
     def get_embeddings(self, dataloader: DataLoader, dict_to_modify: Dict[str, torch.Tensor]) -> None:
         
-        
         if torch.distributed.is_available():
             if self.world_size > 1: device = 'cuda:0'
             else: device = 'cuda' 
         else: device = 'cpu'
 
-        
         checkpoint: Dict = torch.load(f'{self.best_check_filename}/best_{self.method_name}_{device}.pth.tar', map_location=self.device)
         self.model.load_state_dict(checkpoint['state_dict'])
         
         if 'embedds' in dict_to_modify:
-            dict_to_modify['embedds'] = torch.empty((0, self.model.linear.in_features), dtype=torch.float32)#, device=self.device)
+            dict_to_modify['embedds'] = torch.empty((0, self.model.linear.in_features), dtype=torch.float32, device=self.device)
         if 'probs' in dict_to_modify:
-            dict_to_modify['probs'] = torch.empty((0, self.n_classes), dtype=torch.float32)#, device=self.device)
+            dict_to_modify['probs'] = torch.empty((0, self.n_classes), dtype=torch.float32)
         if 'labels' in dict_to_modify:
-            dict_to_modify['labels'] = torch.empty(0, dtype=torch.int8)#, device=self.device)
+            dict_to_modify['labels'] = torch.empty(0, dtype=torch.int8)
         if 'idxs' in dict_to_modify:
-            dict_to_modify['idxs'] = torch.empty(0, dtype=torch.int8)#, device=self.device)
+            dict_to_modify['idxs'] = torch.empty(0, dtype=torch.int8)
         
         self.model.eval()
 
@@ -88,31 +86,19 @@ class TrainEvaluate(object):
         with torch.inference_mode():
             for idxs, images, labels in dataloader:
                 
-                #f('embedds' in dict_to_modify or 'probs' in dict_to_modify):
-                #    outs, embed, _, _ = self.model(images.to(self.device))
-                
                 if 'embedds' in dict_to_modify:
                     _, embed, _, _ = self.model(images.to(self.device))
-                    dict_to_modify['embedds'] = torch.cat((dict_to_modify['embedds'], embed.squeeze().cpu()), dim=0)
-                    #self.clear_cuda_variables([embed])
-                    #del embed
+                    dict_to_modify['embedds'] = torch.cat((dict_to_modify['embedds'], embed.squeeze()), dim=0)
                     
                 if 'probs' in dict_to_modify:
                     outs, _, _, _ = self.model(images.to(self.device))
                     dict_to_modify['probs'] = torch.cat((dict_to_modify['probs'], outs.squeeze().cpu()), dim=0)
-                    #self.clear_cuda_variables([outs])
-                    #del outs
                     
                 if 'labels' in dict_to_modify:
-                    dict_to_modify['labels'] = torch.cat((dict_to_modify['labels'], labels), dim=0)#.to(self.device)
+                    dict_to_modify['labels'] = torch.cat((dict_to_modify['labels'], labels), dim=0)
                 if 'idxs' in dict_to_modify:
-                    dict_to_modify['idxs'] = torch.cat((dict_to_modify['idxs'], idxs), dim=0)#.to(self.device)
+                    dict_to_modify['idxs'] = torch.cat((dict_to_modify['idxs'], idxs), dim=0)
 
-                
-        #self.clear_cuda_variables([checkpoint])
-        #del checkpoint
-        #gc.collect()
-        #torch.cuda.empty_cache()
         
         
         

app/utils.py
@@ -185,7 +185,6 @@ def save_train_val_curves(results_info: Dict[str, Any], ts_dir: str, dataset_nam
 
     plt.suptitle(f'AL iter {cicle_iter}.{al_iter} - {results_info["model_name"]}', fontsize = 30)
     
-    #dataset_name
     path_plots = f'results/{ts_dir}/{dataset_name}/{cicle_iter}/train_val_plots/{results_info["model_name"]}'
 
     if(not os.path.exists(path_plots)):
@@ -215,6 +214,7 @@ def create_directory(dir: str) -> None:
     if not os.path.exists(dir):
         os.makedirs(dir)
 
+
             
 def create_ts_dir_res(timestamp: str, dataset_name: str, iter: str) -> None:
     mydir = os.path.join('results', timestamp)
@@ -223,6 +223,7 @@ def create_ts_dir_res(timestamp: str, dataset_name: str, iter: str) -> None:
     create_directory(os.path.join(mydir, dataset_name, iter))
     create_directory(os.path.join(mydir, dataset_name, iter, 'train_val_plots'))
     create_directory(os.path.join(mydir, dataset_name, iter, 'tsne_plots'))
+    
     
     
         
@@ -272,40 +273,36 @@ def plot_history(path: str, labels: List[int], classes: List[str], method_name: 
     
     
     
-def plot_derivatives(method_name: str, der_tensor: torch.Tensor, der_weighted_tensor: torch.Tensor, path: str, labels: List[int], classes: List[str], iter: int, max_x: int, dir: str) -> None:   
+def plot_derivatives(method_name: str, der_tensor: torch.Tensor, path: str, labels: List[int], classes: List[str], iter: int, max_x: int, dir: str) -> None:   
     
     create_directory(f'{path}/{method_name}')
     create_directory(f'{path}/{method_name}/{dir}')
     
-    fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (20,8))
+    plt.figure(figsize=(10,8))
     
     x = np.arange(max_x)
     palette = list(mcolors.TABLEAU_COLORS.values())
-    
-    for idx, (tensor, title) in enumerate(zip([der_tensor, der_weighted_tensor], ['Derivatives', 'Weighted Average Derivatives'])):
+            
+    der_tensor = der_tensor.cpu().numpy()
         
-        tensor = tensor.cpu().numpy()
-        
-        plotted_classes = set()
+    plotted_classes = set()
 
-        for i, lab in zip(range(len(tensor)), labels):
-            if lab not in plotted_classes:
-                ax[idx].plot(x, tensor[i], linestyle="-", label=classes[lab], color=palette[lab])
-                plotted_classes.add(lab)
-            else:
-                ax[idx].plot(x, tensor[i], linestyle="-", color=palette[lab])
+    for i, lab in zip(range(len(der_tensor)), labels):
+        if lab not in plotted_classes:
+            plt.plot(x, der_tensor[i], linestyle="-", label=classes[lab], color=palette[lab])
+            plotted_classes.add(lab)
+        else:
+            plt.plot(x, der_tensor[i], linestyle="-", color=palette[lab])
                 
-        ax[idx].set_xlabel('GTG Iterations')
-        ax[idx].set_ylabel('Entropy')
-        ax[idx].grid()
-        ax[idx].legend()
-        ax[idx].set_title(title)
-        
-    plt.suptitle(f'{title} - Iteration {iter}', fontsize = 15)
+    plt.xlabel('GTG Iterations')
+    plt.ylabel('Entropy')
+    plt.grid()
     plt.legend()
+    plt.title(f'Derivatives - Iteration {iter}')
+
     plt.savefig(f'{path}/{method_name}/{dir}/{iter}.png')
-    
-    
+
+
 
 
 def plot_accuracy_std_mean(timestamp: str, dataset_name: str) -> None:
@@ -339,12 +336,12 @@ def plot_accuracy_std_mean(timestamp: str, dataset_name: str) -> None:
     
     plt.savefig(f'results/{timestamp}/{dataset_name}/mean_std_accuracy_plot.png') 
     
+    
 
 class Entropy_Strategy(Enum):
-    MEAN_DER = 0
-    W_A_DER = 1
-    H_INT = 2
-    LAST = 3
+    DER = 0
+    H_INT = 1
+
 
 
 def set_seeds(seed: int = 10001) -> None:
@@ -378,9 +375,8 @@ def plot_tsne_A(A: Tuple[torch.Tensor], labels: Tuple[torch.Tensor], classes: Li
     
     A_1, A_2 = A
     
-    label_lab = labels[0].cpu().numpy()
+    label_lab, unlabel_lab = labels
     len_lab = len(label_lab)
-    unlabel_lab = labels[1].cpu().numpy()
     
     tsne_A1 = TSNE().fit_transform(A_1.cpu().numpy())
     tsne_A2 = TSNE().fit_transform(A_2.cpu().numpy())
@@ -389,30 +385,20 @@ def plot_tsne_A(A: Tuple[torch.Tensor], labels: Tuple[torch.Tensor], classes: Li
         
     for idx, (tsne, name) in enumerate(zip([tsne_A1, tsne_A2], ['Uncertanity', f'{strategy} Sparsification'])):
         
-        tsne_lab = tsne[:len_lab,:]
-        tsne_unlab = tsne[len_lab:,:]
-        
-        x_lab = tsne_lab[:,0]
-        y_lab = tsne_lab[:,1]
-        
-        x_unlab = tsne_unlab[:,0]
-        y_unlab = tsne_unlab[:,1]
-        
-        x = np.hstack((x_lab, x_unlab))
-        y = np.hstack((y_lab, y_unlab))
+        tsne_lab, tsne_unlab = tsne[:len_lab,:], tsne[len_lab:,:]
+        x_lab, y_lab = tsne_lab[:,0], tsne_lab[:,1]
+        x_unlab, y_unlab = tsne_unlab[:,0], tsne_unlab[:,1]
+        x, y = np.hstack((x_lab, x_unlab)), np.hstack((y_lab, y_unlab))
         label = np.hstack((label_lab, unlabel_lab))
         
-    
         sns.scatterplot(x=x_unlab, y=y_unlab, label='unlabeled', color='blue', ax=axes[idx][0])
         sns.scatterplot(x=x_lab, y=y_lab, label='labeled', color='orange', ax=axes[idx][0])
         axes[idx][0].set_title(f'{name} Affnity Matrix')
         axes[idx][0].legend()
         
-        
         sns.scatterplot(x=x, y=y, hue=[classes[l] for l in label], ax=axes[idx][1])
         axes[idx][1].set_title(f'{name} Affnity Matrix - Classes')
         axes[idx][1].legend()
-    
 
     plt.suptitle('Affinity TSNE plots')
     
@@ -426,23 +412,12 @@ def plot_new_labeled_tsne(lab: Dict[str, torch.Tensor], unlab: Dict[str, torch.T
     
     tsne = TSNE().fit_transform(np.vstack((lab['embedds'].cpu().numpy(), unlab['embedds'].cpu().numpy())))
     
-    tsne_lab = tsne[:len(lab['embedds']),:]
-    tsne_unlab = tsne[len(lab['embedds']):,:]
-    
-    label_lab = lab['labels'].cpu().numpy()
-    unlabel_lab = unlab['labels'].cpu().numpy()
-    
-    x_lab = tsne_lab[:,0]
-    y_lab = tsne_lab[:,1]
-    
-    x_unlab = tsne_unlab[:,0]
-    y_unlab = tsne_unlab[:,1]
-        
-    x = np.hstack((x_lab, x_unlab))
-    y = np.hstack((y_lab, y_unlab))
-    
+    tsne_lab, tsne_unlab = tsne[:len(lab['embedds']),:], tsne[len(lab['embedds']):,:]
+    label_lab, unlabel_lab = lab['labels'], unlab['labels']
+    x_lab, y_lab = tsne_lab[:,0],tsne_lab[:,1]
+    x_unlab, y_unlab = tsne_unlab[:,0], tsne_unlab[:,1]
+    x, y = np.hstack((x_lab, x_unlab)), np.hstack((y_lab, y_unlab))
     label = np.hstack((label_lab, unlabel_lab))
-    
     
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(23, 12))
     
@@ -460,6 +435,18 @@ def plot_new_labeled_tsne(lab: Dict[str, torch.Tensor], unlab: Dict[str, torch.T
     plt.suptitle(f'{ds_name} - {method} - {iter - 1}')
     create_directory(f'results/{time_stamp}/{ds_name}/{samp_iter}/tsne_plots/{method}')
     plt.savefig(f'results/{time_stamp}/{ds_name}/{samp_iter}/tsne_plots/{method}/{iter - 1}.png')
+    
+    
+    
+def count_class_observation(classes, dataset, topk_idx_obs=None):
+    if topk_idx_obs == None: labels = [lab for _,_,lab in dataset]
+    else: labels = [dataset[k][2] for k in topk_idx_obs]
+    
+    d_labels = {}
+    for cls in classes: d_labels[cls] = 0
+    keys_d_labels = list(d_labels.keys())
+    for l in labels: d_labels[keys_d_labels[l]] += 1
+    return d_labels
     
     
     